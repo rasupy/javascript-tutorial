0070d0502bfec51d5a923e7d82010e9cdd100e47
rasupy <lyukey0902@gmail.com>
2025-08-12 13:51:05 +0900

    practice(javascript): 関数の定義

---
+++ study/file_6/p_02.js
@@ -0,0 +1,6 @@
+function multiply( x, y ) {
+    return x * y;
+}
+
+console.log( multiply( 7, 9 ) );
+console.log( multiply( -11, 9 ) );
---
+++ study/file_6/p_05.js
@@ -0,0 +1,8 @@
+function printHello() {
+    hello = "Hello, World!";
+    return hello;
+}
+
+prop = printHello();
+console.log( prop );
+
