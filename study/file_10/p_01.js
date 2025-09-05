a997aebc666c9a218183c5d071278aab3e1c7ea4
raspy <lyukey0902@gmail.com>
2025-09-06 08:51:10 +0900

    practice(js): 配列について

---
+++ study/file_10/p_01.js
@@ -0,0 +1,16 @@
+// 2次元配列
+const array2D = [
+  ["太郎", "25歳", "男"],
+  { name: "花子", age: "22歳", gender: "女" },
+];
+
+console.log(array2D[0]);
+// [ '太郎', '25歳', '男' ]
+console.log(array2D[0][0]);
+// 太郎
+console.log(array2D[0][2]);
+// 男
+console.log(array2D[1].name);
+// 花子
+console.log(array2D[1].age);
+// 22歳
