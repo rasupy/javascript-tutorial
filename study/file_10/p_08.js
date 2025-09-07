55a13a6969c8e7d71bee936a2b9d710d8a3c7d65
raspy <lyukey0902@gmail.com>
2025-09-07 15:23:32 +0900

    practice(js): 配列について

---
+++ study/file_10/p_08.js
@@ -0,0 +1,7 @@
+const arry = [1, 2, 3, 4, 5];
+
+const result1 = arry.every((value) => value > 0);
+console.log(result1); // true
+
+const result2 = arry.every((value) => value > 3);
+console.log(result2); // false
