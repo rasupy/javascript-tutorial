e48e738f9ae53added5216eb73aceb6464cd1bcd
raspy <lyukey0902@gmail.com>
2025-10-16 13:04:24 +0900

    practice(js): innerHTMLについて

---
+++ study/file_13/p_04.js
@@ -0,0 +1,8 @@
+// innerHTML
+const testEl = document.querySelector("#test");
+
+console.log(`innerHTML: ${testEl.innerHTML}`);
+
+setTimeout(() => {
+  testEl.innerHTML = "<h1>Good World</h1>";
+}, 2000);
